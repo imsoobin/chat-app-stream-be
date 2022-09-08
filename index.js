35c103521b1bf2383ad1452b4d8c87d6b69ff0d7
PhamSon1710 <sonqp@jitsinnovationlabs.com>
2022-09-08 14:41:43 +0700

    test lan 2

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
-const app = express();
-const server = http.createServer(app);
+// const http = require("http");
+// const server = http.createServer(app);
 
+const app = express();
 const authRoutes = require("./routes/auth");
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000" || "https://ohmess.netlify.app",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "http://localhost:3000" || "https://ohmess.netlify.app",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    });
-  });
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
-});
+// io.on("connection", (socket) => {
+//   socket.emit("me", socket.id);
+//   socket.on("disconnect", () => {
+//     socket.broadcast.emit("callEnded");
+//   });
+//   socket.on("callUser", (data) => {
+//     io.to(data.userToCall).emit("callUser", {
+//       signal: data.signalData,
+//       from: data.from,
+//       name: data.name,
+//     });
+//   });
+//   socket.on("answerCall", (data) => {
+//     io.to(data.to).emit("callAccepted", data.signal);
+//   });
+// });
 
 const PORT = process.env.PORT || 5001;
 
