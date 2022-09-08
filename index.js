b2a848b24b2cfe24171cca4fb262b7034198e0e3
PhamSon1710 <sonqp@jitsinnovationlabs.com>
2022-09-08 15:08:43 +0700

    test lan 4

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-// const http = require("http");
-// const server = http.createServer(app);
-
+const http = require("http");
 const app = express();
+const server = http.createServer(app);
+
 const authRoutes = require("./routes/auth");
 
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: "http://localhost:3000" || "https://ohmess.netlify.app",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-// io.on("connection", (socket) => {
-//   socket.emit("me", socket.id);
-//   socket.on("disconnect", () => {
-//     socket.broadcast.emit("callEnded");
-//   });
-//   socket.on("callUser", (data) => {
-//     io.to(data.userToCall).emit("callUser", {
-//       signal: data.signalData,
-//       from: data.from,
-//       name: data.name,
-//     });
-//   });
-//   socket.on("answerCall", (data) => {
-//     io.to(data.to).emit("callAccepted", data.signal);
-//   });
-// });
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
 
-// const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 require("dotenv").config();
 
@@ -74,8 +74,8 @@ app.post("/", (req, res) => {
 app.use("/auth", authRoutes);
 
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-const server = app.listen(process.env.PORT || 5001, () => {
-  const port = server.address().port;
-  console.log(`Express is working on port ${port}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const server = app.listen(process.env.PORT || 5001, () => {
+//   const port = server.address().port;
+//   console.log(`Express is working on port ${port}`);
+// });
